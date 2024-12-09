561c1282fb3556d03e58bc7f123c4386ab421c6b
Rasheed-Yusuf <149545750+Rasheed-Yusuf@users.noreply.github.com>
2024-12-09 13:57:00 -0800

    Update MockFirmwareVolumeBlock.h
    Fixes for CI Build errors
    
    Signed-off-by: Rasheed-Yusuf <149545750+Rasheed-Yusuf@users.noreply.github.com>

--- MdePkg/Test/Mock/Include/GoogleTest/Protocol/MockFirmwareVolumeBlock.h
+++ MdePkg/Test/Mock/Include/GoogleTest/Protocol/MockFirmwareVolumeBlock.h
@@ -26,56 +26,56 @@ struct MockEfiFirmwareVolumeBlockProtocol {
     EFI_STATUS,
     GetAttributes,
     (
-      IN CONST  EFI_FIRMWARE_VOLUME_BLOCK2_PROTOCOL* This,
-      OUT       EFI_FVB_ATTRIBUTES_2* Attributes)
+     IN CONST  EFI_FIRMWARE_VOLUME_BLOCK2_PROTOCOL* This,
+     OUT       EFI_FVB_ATTRIBUTES_2* Attributes)
     );
 
   MOCK_FUNCTION_DECLARATION (
     EFI_STATUS,
     SetAttributes,
     (
-      IN CONST  EFI_FIRMWARE_VOLUME_BLOCK2_PROTOCOL* This,
-      IN OUT    EFI_FVB_ATTRIBUTES_2* Attributes)
+     IN CONST  EFI_FIRMWARE_VOLUME_BLOCK2_PROTOCOL* This,
+     IN OUT    EFI_FVB_ATTRIBUTES_2* Attributes)
     );
 
   MOCK_FUNCTION_DECLARATION (
     EFI_STATUS,
     GetPhysicalAddress,
     (
-      IN CONST  EFI_FIRMWARE_VOLUME_BLOCK2_PROTOCOL* This,
-      OUT       EFI_PHYSICAL_ADDRESS* Address)
+     IN CONST  EFI_FIRMWARE_VOLUME_BLOCK2_PROTOCOL* This,
+     OUT       EFI_PHYSICAL_ADDRESS* Address)
     );
 
   MOCK_FUNCTION_DECLARATION (
     EFI_STATUS,
     GetBlockSize,
     (
-      IN CONST  EFI_FIRMWARE_VOLUME_BLOCK2_PROTOCOL* This,
-      IN        EFI_LBA                             Lba,
-      OUT       UINTN* BlockSize,
-      OUT       UINTN* NumberOfBlocks)
+     IN CONST  EFI_FIRMWARE_VOLUME_BLOCK2_PROTOCOL* This,
+     IN        EFI_LBA                             Lba,
+     OUT       UINTN* BlockSize,
+     OUT       UINTN* NumberOfBlocks)
     );
 
   MOCK_FUNCTION_DECLARATION (
     EFI_STATUS,
     Read,
     (
-      IN CONST  EFI_FIRMWARE_VOLUME_BLOCK2_PROTOCOL* This,
-      IN        EFI_LBA                             Lba,
-      IN        UINTN                               Offset,
-      IN OUT    UINTN* NumBytes,
-      IN OUT    UINT8* Buffer)
+     IN CONST  EFI_FIRMWARE_VOLUME_BLOCK2_PROTOCOL* This,
+     IN        EFI_LBA                             Lba,
+     IN        UINTN                               Offset,
+     IN OUT    UINTN* NumBytes,
+     IN OUT    UINT8* Buffer)
     );
 
   MOCK_FUNCTION_DECLARATION (
     EFI_STATUS,
     Write,
     (
-      IN CONST  EFI_FIRMWARE_VOLUME_BLOCK2_PROTOCOL* This,
-      IN        EFI_LBA                             Lba,
-      IN        UINTN                               Offset,
-      IN OUT    UINTN* NumBytes,
-      IN        UINT8* Buffer)
+     IN CONST  EFI_FIRMWARE_VOLUME_BLOCK2_PROTOCOL* This,
+     IN        EFI_LBA                             Lba,
+     IN        UINTN                               Offset,
+     IN OUT    UINTN* NumBytes,
+     IN        UINT8* Buffer)
     );
 };
 
@@ -98,4 +98,3 @@ MOCK_FUNCTION_DEFINITION (MockEfiFirmwareVolumeBlockProtocol, Write, 5, EFIAPI);
  EFI_FIRMWARE_VOLUME_BLOCK_PROTOCOL  *NAME = &NAME##_INSTANCE;
 
 #endif // MOCK_FIRMWARE_VOLUME_BLOCK_H_
-
